15ec4cff48a8ef200055d461e7ff60efa5035834
Jackie <jeekieb@gmail.com>
2018-02-22 16:10:18 -0500

    Added what I think is the way to connect to DB

--- PHP/main.php
+++ PHP/main.php
@@ -4,3 +4,31 @@
  * Date: 2/18/18
  * Time: 11:32 PM
  */
+ 
+$servername = "localhost";
+$username = "pkMUser";
+$password = "Fellowship123";
+$dbName
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbName);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+// DML
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+ 
+ ?>
