98e31453450b86917bf096fc2814628eb6b62d1a
Jackie <jeekieb@gmail.com>
2018-03-12 22:59:50 -0400

    added shop page and connected to DB-error

---
+++ PHP/shopTest.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+
+$db = new PDO('mysql:host=localhost;dbname=pkjewelers', 'fellowship', 'Ns42Wdu93J3lwgC');
+$qr = $db->prepare("SELECT * FROM Inventory");
+echo $qr;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shop </title>
+    <link rel="stylesheet" type="text/css" href="../CSS/bulmaswatch.min.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+    <link rel="icon" href="../ASSETS/favicon-diamond.ico">
+    <script src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
+</head>
+<body>
+<section class="hero is-white is-fullheight">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+        <header class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item">
+                        <h1 class="subtitle">PK<i class="far fa-gem fa-sm"></i></h1>
+                    </a>
+                    <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    </span>
+                </div>
+                <div id="navbarMenuHeroC" class="navbar-menu has-text-centered">
+                    <div class="navbar-end">
+                        <a href="../index.html" class="navbar-item underline">
+                            Home
+                        </a>
+						<a class="navbar-item underline">
+                            Shop
+                        </a>
+                        <a href="PHP/signIn.php" class="navbar-item underline">
+                            Sign In
+                        </a>
+                        <a class="navbar-item underline">
+                            <i class="fa fa-shopping-cart fa-lg"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <div class="column is-4 is-offset-4">
+                <h3 class="title has-text-grey">Shop</h3>
+                <h2 style="color: red;"><span><?php echo $output; ?></span></h2>
+                <div class="box">
+                    <form name="form-shopTest" method="POST" action="./shopTest.php">
+                        <div class="field">
+                            <div class="control">
+                                <input class="input is-large" type="text" placeholder="Item" name="item"
+                                       id="email" autofocus="">
+                            </div>
+                        </div>
+                        <button class="button is-block is-success is-large is-fullwidth">Search</button>
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+</body><!--<script async type="text/javascript" src="../js/bulma.js"></script>-->
+<script type="text/javascript">
+    document.addEventListener('DOMContentLoaded', function () {
+
+        // Get all "navbar-burger" elements
+        var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+
+            // Add a click event on each of them
+            $navbarBurgers.forEach(function ($el) {
+                $el.addEventListener('click', function () {
+
+                    // Get the target from the "data-target" attribute
+                    var target = $el.dataset.target;
+                    var $target = document.getElementById(target);
+
+                    // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                    $el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+
+                });
+            });
+        }
+
+    });
+</script>
+<!--<script src="../JS/instantclick.min.js" data-no-instant></script>-->
+<!--<script data-no-instant>InstantClick.init();</script>-->
+</html>
