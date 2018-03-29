06fbe7ba512596f69935a7f7f6fc86f3a711e326
Jash Vaidya <jashvaidya7x@gmail.com>
2018-03-28 21:22:04 -0400

    fixes

---
+++ PHP/test.php
@@ -0,0 +1,50 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/bulmaswatch.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <link rel="icon" href="../ASSETS/favicon-diamond.ico">
+    <title>Shop</title>
+</head>
+
+<body>
+<section class="hero is-white is-fullheight" id="landing-page">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+        <header class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item">
+                        <h1 class="title is-2 is-blue">PK<i class="far fa-gem fa-sm"></i></h1>
+                    </a>
+                    <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                    </span>
+                </div>
+                <div id="navbarMenuHeroC" class="navbar-menu has-text-centered">
+                    <div class="navbar-end">
+                        <a href="../index.html" class="navbar-item underline">
+                            Home
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+
+<!--    <iframe style="width: 1px; min-width: 100%;" scrolling="no" width="100%" height="1500px" frameborder="0" src="https://pkjewelers-frame-categoryembed.jewelershowcase.com/"></iframe>-->
+    <iframe  scrolling="no" width="100%" height="1500px"  src="https://pkjewelers-frame-categoryembed.jewelershowcase.com/"></iframe>
+
+</section>
+
+</html>
