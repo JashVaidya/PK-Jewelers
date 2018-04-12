6ea33ceb8ca1814643f96b0305787272d877efb7
Brandon <bwoodby@radford.edu>
2018-04-12 01:19:42 -0400

    Fixed contact.php

--- PHP/contact.php
+++ PHP/contact.php
@@ -1,20 +1,24 @@
-<!DOCTYPE html>
-<?php 
-	if(!isset($_POST["email"])))
-		echo "Please provide a valid email address.";
-	if(!isset($_POST["name"]))
-		echo "Please provide your name.";
-	if(!isset($_POST["message"]))
-		echo "We want to hear from you.  Please provide a message";
-	$company_email = "jashvaidya7x@gmail.com";
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$message = $_POST["message"];
-	$submitted = "Name: " . $name . "\n";
-	$submitted .= "Email: " . $email . "\n";
-	$submitted .= "Message: " . $message . "\n";
-	mail($company_email, $name . " Comments", $submitted);
+<?php
+	if(isset($_POST["email"]))
+	{
+		$company_email = "bwoodby@radford.edu";
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		$message = $_POST["message"];
+		$submitted = "Name: " . $name . "\n";
+		$submitted .= "Email: " . $email . "\n";
+		$submitted .= "Message: " . $message . "\n";
+		$subject = $name . " Comments";
+		$header = "From: " . $name;
+		$mail = mail($company_email, $subject, $submitted, $header);
+		if($mail)
+			echo "success";
+		else
+			echo "fail";
+	}
+	
 ?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -62,7 +66,7 @@
                           <input type="hidden" name="cmd" value="_s-xclick">
                           <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAsFkcHY1HMerlchBwWcOzBqdfNmPWTjIR/x5K9FftGkE6TmVePwKQe/dggokPP92yxTQoYjRExF8r27XBbqxAFl6uZJsfkU4+/s51IaLhKT5+dduRRDTQJgxuxfWcdLxng4ovgIExZaxyG5sWkKBmHgaXHtYvGYFjzEZnLqzBVmDELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAhHc8P9acTdFoAw1l4QJ4n9uVJKgNocKkJb84rsas6mRb0695TgaJo2n30Gzw9AtzMBReFa2RwYSXFtoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTgwNDExMTIyMDU5WjAjBgkqhkiG9w0BCQQxFgQU/8rKxiN+ZLVz8EAONwId6vSJ8vswDQYJKoZIhvcNAQEBBQAEgYCIKsEVu1B7Sifo4w9pkOdQU3PSXC4cqQVQKAv/vsoGHkbk/iC+EpSDsJpDfWzpOtrYRtbQQp7PY45mR7R4dfOpp82icpBpGSliTyJggURwE/H4nDf4n5LHbQTqCiHDPzwtyKHEdZ1aKjwYrdqp/NSo+5TMeqHDwLpGfKWS/iPJMQ==-----END PKCS7-----
                           ">
-                          <input type="image" src="ASSETS/bag.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                          <input type="image" src="../ASSETS/bag.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                           <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
                           </form>
                         </a>
@@ -77,11 +81,11 @@
 <div class="container has-text-centered">
 
     <h1 class="title ">Let's Get In Touch</h1>
-<form method="post">
+<form action="contact.php" method="post">
 	<div class="field has-addons has-addons-centered">
 		<label class="label is-large">Name </label>
 		<div class="control has-icons-left has-icons-right">
-			<input id="name" class="input" type="text" placeholder="e.g Alex Smith">
+			<input id="name" name="name" class="input" type="text" placeholder="e.g Alex Smith">
 			<span class="icon is-small is-left">
 				<i class="fas fa-user"></i>
 			</span>
@@ -91,7 +95,7 @@
 	<div class="field has-addons has-addons-centered">
 		<label class="label is-large">Email </label>
 		<p class="control has-icons-left has-icons-right">
-			<input id="email" class="input" type="email" placeholder="Email">
+			<input id="email" name="email" class="input" type="email" placeholder="Email">
 			<span class="icon is-small is-left">
 		<i class="fas fa-envelope"></i>
 		</span>
@@ -106,7 +110,7 @@
         <div class="field-body">
             <div class="field">
                 <div class="control">
-                    <textarea id="message" class="textarea" placeholder="Explain how we can help you"></textarea>
+                    <textarea id="message" name="message" class="textarea" placeholder="Explain how we can help you"></textarea>
                 </div>
             </div>
         </div>
@@ -116,9 +120,7 @@
 
             <div class="field">
                 <div class="control">
-                    <button type="submit" class="button is-info">
-                        Send message
-                    </button>
+                    <input type="submit" class="button is-info">
                 </div>
             </div>
     </div>
@@ -140,7 +142,7 @@
 
 
 </body>
-<script src="JS/main.js" type="text/javascript"></script>
+<script src="../JS/main.js" type="text/javascript"></script>
 <!--<script src="JS/instantclick.min.js" data-no-instant></script>-->
 <!--<script data-no-instant>InstantClick.init();</script>-->
 </html>
